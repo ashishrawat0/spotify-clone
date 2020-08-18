a8d1f52a0e9fcb992d3614cb63336399e0d295ba
ashishrawat0 <rawatchd@hotmail.com>
2020-08-18 14:14:06 +0530

    herf added to login page

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import "./Login.css"
+import { loginUrl } from './spotify'
 function Login() {
     return (
         <div className="login">
             {/*  logo */}
             {/*  spotify  login system */}
             <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""></img>
-            <a>Login with spotify</a>
+            <a href={loginUrl}>Login with spotify</a>
         </div>
     )
 }
