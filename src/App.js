958e9b811ac6754648328ed909c6f91a99a5c09f
ashishrawat0 <rawatchd@hotmail.com>
2020-08-18 16:57:21 +0530

    if condition added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React , { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import {getTokenFromUrl} from './spotify'
+import { getTokenFromUrl } from './spotify'
 import Login from './Login';
 
 function App() {
-  const [token,setToken] = useState(null)
-  useEffect(()=>{
-      const hash = getTokenFromUrl();
-      window.location.hash = "";
-      const _token = hash.access_token
-      if(_token){
-        setToken(_token);
-      }
-      
-  },[])
+  const [token, setToken] = useState(null)
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token
+    if (_token) {
+      setToken(_token);
+    }
+
+  }, [])
 
   return (
     <div className="app">
-      <Login/>
+      <Login />
     </div>
   );
 }
