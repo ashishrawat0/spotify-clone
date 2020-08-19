61e35683a157f123e983575d1eaacc0b1cc7bdf0
ashishrawat0 <rawatchd@hotmail.com>
2020-08-20 00:18:12 +0530

    token now saved in context api

--- src/App.js
+++ src/App.js
@@ -3,28 +3,42 @@ import './App.css';
 import { getTokenFromUrl } from './spotify'
 import Login from './Login';
 import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player'
+import {useDataLayerValue} from './DataLayer'
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null)
+  const [{user, token },dispatch] = useDataLayerValue();
+
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token
     if (_token) {
-      setToken(_token);
-      spotify.setAccessToken(_token)
-      spotify.getMe().then((user)=>{
-        console.log('user info ->',user);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
       })
 
-    }
+      spotify.setAccessToken(_token)
 
+      spotify.getMe().then((user) => {  
+            dispatch({
+              type:"SET_USER",
+              user:user
+            })
+      });
+    }
   }, [])
+
+  console.log(token)
+
+
+  // console.log("hey dispacth works", user)
 
   return (
     <div className="app">
-      <Login />
+      {token ? <Player /> : <Login />}
     </div>
   );
 }
