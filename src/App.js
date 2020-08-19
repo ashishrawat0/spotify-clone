f222f7d8bf530269bd14a210fd8a9669f18e8544
ashishrawat0 <rawatchd@hotmail.com>
2020-08-20 02:24:43 +0530

    User playlist added to the data layer

--- src/App.js
+++ src/App.js
@@ -28,6 +28,12 @@ function App() {
           user: user
         })
       });
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        })
+      })
     }
   }, [])
   return (
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,13 +4,21 @@ import SideBarOption from './SideBarOption'
 import HomeIcon from '@material-ui/icons/Home'
 import SearchIcon from '@material-ui/icons/Search'
 import LibraryMusicIcon from '@material-ui/icons/LibraryMusic'
+import { useDataLayerValue } from './DataLayer'
+
+
 function Sidebar() {
+    const [{ playlists }, dispatch] = useDataLayerValue();
     return (
         <div className="sidebar">
             <img className="sidebar__logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""></img>
             <SideBarOption Icon={HomeIcon} title="Home"/>
             <SideBarOption Icon={SearchIcon} title="Search"/>
             <SideBarOption Icon={LibraryMusicIcon} title="Your Library"/>
+            <br></br>
+            <strong className="sidebar__title">PLAYLIST</strong>
+            <hr/>
+            <SideBarOption title={playlists[0]}/>
         </div>
     )
 }
