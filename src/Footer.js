97e0c57230ce41b095bf984d670f0c8484cf919c
ashishrawat0 <rawatchd@hotmail.com>
2020-08-21 23:22:41 +0530

    added footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,30 @@
 import React from 'react'
-import './Footer.css'
+import './Footer.css'   
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+
 function Footer() {
     return (
         <div className="footer">
-            <h1>I am the Footer</h1>
+            <div className="footer__left">
+                <p>Album sing and details</p>
+            </div>
+            <div className="footer__center">
+                <ShuffleIcon className="footer__green"/>
+                <SkipPreviousIcon className="footer__icon" />
+                <PlayCircleOutlineIcon fontSize="large" className=""/>
+                <SkipNextIcon className="footer__icon"/>
+                <RepeatIcon className="footer__green"/>
+            </div>
+            <div className="footer__right">
+                <p>Volume controls</p>
+            </div>
         </div>
     )
 }
